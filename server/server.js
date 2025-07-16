f53d148ef6e2bccc5aea847dc8abd48d3210fa2b
gh4aniket <116141692+gh4aniket@users.noreply.github.com>
2025-07-16 19:47:47 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,10 +17,10 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
-if (process.env.NODE_ENV==='production')
+/*if (process.env.NODE_ENV==='production')
 {
     app.use(express.static("client/build"));
-}
+}*/
 const PORT =process.env.PORT|| 8000;
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
 const USERNAME = process.env.DB_USERNAME;
